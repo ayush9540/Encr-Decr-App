c0e6aed7ebb9ad3ae22e437d8084ca6c37fccd65
ayush9540 <018.ayush@gmail.com>
2019-09-05 02:42:07 +0530

    overview added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,11 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'login';
-// }
-
-
-import { Component } from '@angular/core';  
-import * as CryptoJS from 'crypto-js';
+import { Component } from '@angular/core';
   
 @Component({  
   selector: 'app-root',  
   templateUrl: './app.component.html',  
   styleUrls: ['./app.component.css']  
-})  
+}) 
+ 
 export class AppComponent {  
-  title = 'EncryptionDecryptionSample';  
-    
-  plainText:string;  
-  encryptText: string;  
-  encPassword: string;  
-  decPassword:string;  
-  conversionEncryptOutput: string;  
-  conversionDecryptOutput:string;  
-  bulbon: boolean;
-  names = [];
-  constructor() {  
-    
-  }  
-  //method is used to encrypt and decrypt the text  
-  convertText(conversion:string) {  
-      if (conversion=="encrypt") {  
-        this.conversionEncryptOutput = CryptoJS.AES.encrypt(this.plainText.trim(), this.encPassword.trim()).toString();  
-      }  
-      else {  
-        this.conversionDecryptOutput = CryptoJS.AES.decrypt(this.encryptText.trim(), this.decPassword.trim()).toString(CryptoJS.enc.Utf8);      
-    }  
-  }  
+  title = 'EncryptionDecryptionSample';   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { PracticeComponent } from './practice/practice.component';
 import { ToggleEncDecComponent } from './toggle-enc-dec/toggle-enc-dec.component';
+import { HeaderComponent } from './header/header.component';
+import { EncryptionComponent } from './encryption/encryption.component';
+import { DecryptionComponent } from './decryption/decryption.component';
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PracticeComponent,
-    ToggleEncDecComponent
+    ToggleEncDecComponent,
+    HeaderComponent,
+    EncryptionComponent,
+    DecryptionComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
